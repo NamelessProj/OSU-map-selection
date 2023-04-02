1f766a3a4df783b5fdb5ce53d352a21ef0146f97
Nameless Project <121442866+NamelessProj@users.noreply.github.com>
2023-04-02 16:49:07 +0200

    Update search.js

--- search.js
+++ search.js
@@ -1,19 +1,3 @@
-function width_based_on_position(selectPosition, parentHeight, minWidth=0.5, maxWidth=1){
-    var parentMiddle = parentHeight / 2;
-    var e = (-minWidth) / parentMiddle**2 * (selectPosition - parentMiddle)**2 + maxWidth; // f(x) = y = -0.5/X^2 * (V-X)^2 + 1
-    return e;
-} 
-
-console.log(width_based_on_position(400, 800));
-
-
-var g = document.querySelector('.quiz-wrap');
-console.log(g.clientHeight + ' // ' + g.clientWidth);
-
-
-
-
-
 document.querySelector(document).addEventListener('ready', () => {
     document.querySelector('.field').addEventListener('focus', () => {
         document.querySelector('body').classList.add('is-focus');
